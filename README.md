73e6092fba1432c2a4c9dadaa7c4a5e6d5a4c228
Ian <iandelrazo@gmail.com>
2023-05-06 00:36:00 -0600

    more resolution

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Destiny_Page
 Paginas remasterizadas de proyectos escolares
 Esta web se estara actualizando constantemente para ver mis avances como desarrollador web
+
+
+## Link
+[Pagina](https://onemoreian.github.io/Destiny_Page/)
--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,65 @@
 const retroceso = document.querySelector('#atras');
 const avance = document.querySelector('#adelante');
-var recio = document.getElementById('cambio_imagen_dlc');
-
-
-var fondos = ['../src/osi-estra.jpg','../src/forsaker.jpg','../src/shadowkeep.jpg','../src/beyond.jpg','../src/lightfall.jpg'];
+var imagen = document.getElementById('cambio_imagen_dlc');
+var tDlc = document.getElementById('textDlc').textContent;
+//descripcon de los dlcs
+const texto = {
+    renegados:"Después de años de agitación, El Arrecife ha comenzado a degradarse hacia la anarquía. Cayde-6 y el Guardián (nuestro personaje) son enviados a investigar el área y ayudar a aliviar las principales tensiones de la zona. Al aterrizar en El Arrecife, se produce una fuga masiva en la Presidio de los Ancianos, liberando a algunos de los delincuentes más buscados y peligrosos de nuevo a la galaxia.",
+    bastion:"Cuando los héroes dirigían su atención hacia las fronteras de la galaxia, surgieron nuevas pesadillas. Únete a Eris Morn en la Luna, ármate con nuevo equipamiento excepcional y supera el Jardín Negro y el Foso de la herejía. Debes enfrentar al siniestro mal antes de que surja y envíe a la humanidad a otra Edad Oscura.",
+    luz:"La luna Europa guarda muchos secretos perdidos del pasado, como el oscuro poder de la estasis. Colabora con la Desconocida exo y controla este nuevo poder antes de que Eramis, la kell de la Oscuridad, se lo otorgue a sus caídos. Aumenta tu arsenal, domina la estasis y ve más allá de la Luz.",
+    aniversario:"Celebra tres décadas de juego con un evento que rinde homenaje a las aventuras que hemos compartido. No se nos ocurre una mejor manera de celebrar esta ocasión que continuando nuestra aventura juntos. Así que, con los vientos solares detrás, te invitamos a desenvainar tu Héroe de las eras y venir con nosotros a las estrellas.",
+    lightfall:"El Testigo y su nuevo discípulo ya están aquí. Empieza una aventura en la que descubrirás los hilos escondidos que nos conectan a todos y aprenderás a desenredarlos para crear algo nuevo con ellos. Con este nuevo poder, llénate de fuerza junto a los demás guardianes y triunfen frente a la aniquilación total."
+}
+tDlc = texto.renegados
+var fondos = ['./src/forsaker.jpg','./src/shadowkeep.jpg','./src/beyond.jpg','./src/30aniver.jpg','./src/lightfall.jpg'];
 let lugar = 0;
 
-function cambioImagen(){
-    let recio = document.getElementById('cambio_imagen_dlc');
-    if (lugar == 0) {
-        recio.src = `${fondos[1]}`;
-        console.log('lugar0');
-        lugar+=1;
-    } else if (lugar == 1){
-        recio.src = `${fondos[2]}`;
-        console.log('lugar1');
-        lugar ++;
-    } else if (lugar == 2){
-        recio.src = `${fondos[3]}`;
-        console.log('lugar2');
-        lugar ++;
-    } else if (lugar == 3){
-        recio.src = `${fondos[4]}`;
-        console.log('lugar3');
-        lugar ++;
-    }else if (lugar == 4){
-        recio.src = `${fondos[0]}`;
-        console.log('lugar4');
+
+function adelante(){
+    lugar++;
+    if (lugar > 4) {
         lugar = 0;
+        imagen.src = fondos[lugar];
+    } else {
+        imagen.src = fondos[lugar];
+    }
+    contenido();
+};
+function atras(){
+    lugar--;
+    if (lugar < 0) {
+        lugar = 4;
+        imagen.src = fondos[lugar];
+    } else {
+        imagen.src = fondos[lugar];
+    }
+    contenido();
+}
+
+function contenido(){
+    if (lugar == 0) {
+        tDlc = texto.renegados;
+    } else if (lugar == 1) {
+        tDlc = texto.bastion;
+    } else if (lugar == 2) {
+        tDlc = texto.luz;
+    } else if (lugar == 3) {
+        tDlc = texto.aniversario;
+    } else if (lugar == 4) {
+        tDlc = texto.lightfall;
     }
 }
 
 retroceso.addEventListener('click', () =>{
-    /* if (recio.src == fondos[1]) {
-        recio.src = `${fondos[0]}`;
+    /* if (imagen.src == fondos[1]) {
+        imagen.src = `${fondos[0]}`;
         console.log(`cambiado por ${fondos[0]}`);
     } */
-    cambioImagen();
-    
+    atras();
 })
 
 avance.addEventListener('click', () =>{
-    cambioImagen();
+    adelante();
 })
+
+
